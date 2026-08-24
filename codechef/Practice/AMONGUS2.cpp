f1dc76c21e3c001f4e4a7d3c6db790631a5969ef
Karen Brown <dongpei021@users.noreply.github.com>
2026-08-24 18:57:58 +0800

    Solved: AMONGUS2 (codechef)

---
+++ codechef/Practice/AMONGUS2.cpp
@@ -0,0 +1,8 @@
+// Problem: AMONGUS2
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/INOIPRAC/problems/AMONGUS2?tab=statement
+// Solved on: 2026-08-24T10:57:57.019Z
+
+// source not captured automatically - open the solution page and copy it manually
